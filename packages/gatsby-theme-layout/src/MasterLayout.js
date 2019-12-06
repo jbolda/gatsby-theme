ee495ab828ace090604b5d34163f80ee29eec355
Jacob Bolda <me@jacobbolda.com>
2019-12-06 07:33:39 -0600

    componentize layout components

--- packages/gatsby-theme-layout/src/MasterLayout.js
+++ packages/gatsby-theme-layout/src/MasterLayout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Helmet from "react-helmet";
+import { Flex } from "@jbolda/isolated-theme-ui-components";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
@@ -9,16 +10,20 @@ import { fab } from "@fortawesome/free-brands-svg-icons";
 library.add(fas, far, fab);
 
 const MasterLayout = ({ siteMetadata, children }) => (
-  <div className="MasterLayout is-light">
-    <Helmet
-      defaultTitle={siteMetadata.siteTitle}
-      title={siteMetadata.siteTitle}
-      meta={[
-        { name: `description`, content: siteMetadata.siteDescription },
-        { name: `keywords`, content: `articles` }
-      ]}
-    ></Helmet>
+  <Flex direction="column" sx={{ minHeight: "100vh" }}>
+    <Helmet defaultTitle={siteMetadata.siteTitle}>
+      <title>{siteMetadata.siteTitle}</title>
+      <meta property="description" content={siteMetadata.siteDescription} />
+      <meta property="keywords" content="articles" />
+      <style type="text/css">{`
+        body {
+            margin: 0px;
+            padding: 0px;
+        }
+    `}</style>
+    </Helmet>
     {children}
-  </div>
+  </Flex>
 );
+
 export default MasterLayout;
---
+++ packages/gatsby-theme-layout/src/components/header.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import { Flex, Box, Text, Link } from "@jbolda/isolated-theme-ui-components";
+
+const Header = props => {
+  const [hamburgerActive, toggleHamburgerMenu] = useState(false);
+
+  return (
+    <Box
+      as="header"
+      width="98%"
+      sx={{ margin: "0px", padding: "0px", flexShrink: 0 }}
+    >
+      <Flex as="nav" role="navigation" aria-label="main navigation">
+        <Box sx={{ flexGrow: 1, padding: 0, margin: 2 }}>
+          <Link to="/">
+            {props.logo ? (
+              <Logo
+                icon={
+                  !!props.logo.inverse && props.location.pathname !== "/"
+                    ? props.logo.inverse
+                    : props.logo.data
+                }
+                alt={props.logo.alt}
+              />
+            ) : (
+              <Text>{props.siteMetadata.siteTitle}</Text>
+            )}
+          </Link>
+          {/* {!props.siteMetadata.navLinks ? null : (
+            <button
+              aria-label="menu"
+              aria-expanded={hamburgerActive ? "true" : "false"}
+              onClick={toggleHamburgerMenu}
+            >
+              <span aria-hidden="true" />
+              <span aria-hidden="true" />
+              <span aria-hidden="true" />
+              {hamburgerActive ? "true" : "false"}
+            </button>
+          )} */}
+        </Box>
+        {!props.siteMetadata.navLinks
+          ? null
+          : props.siteMetadata.navLinks[0].text === ""
+          ? null
+          : props.siteMetadata.navLinks.map(link => (
+              <Box key={link.text} width={null} sx={{ padding: 0, margin: 2 }}>
+                <Link to={link.url}>{link.text}</Link>
+              </Box>
+            ))}
+      </Flex>
+    </Box>
+  );
+};
+
+export default Header;
+
+const Logo = ({ icon, alt }) => (
+  <img
+    src={icon}
+    alt={alt}
+    style={{
+      height: `50px`,
+      maxHeight: `50px`,
+      marginBottom: `0px`
+    }}
+  />
+);
