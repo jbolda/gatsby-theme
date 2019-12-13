3c2cb4f86540029c766f4f2ecfe70f65ad6e4d85
Jacob Bolda <me@jacobbolda.com>
2019-12-13 08:02:56 -0600

    avatar on homepage

--- packages/gatsby-theme-homepage/src/components/landing.js
+++ packages/gatsby-theme-homepage/src/components/landing.js
@@ -2,24 +2,21 @@ import React from "react";
 // import Img from "gatsby-image";
 import { StaticQuery, graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
-import { Img } from "gatsby-image";
+import Img from "gatsby-image";
 import { Flex, Box } from "@jbolda/isolated-theme-ui-components";
 
 const Landing = ({ landing, profile }) => (
-  <Flex>
+  <Flex sx={{ justifyContent: "space-evenly" }}>
     {!profile ||
     !profile.childImageSharp ||
     !profile.childImageSharp.fluid ? null : (
-      <Box>
-        <Img
-          className="image"
-          Tag="figure"
-          fluid={profile.childImageSharp.fluid}
-        />
+      <Box width={["80%", "50%", "30%"]}>
+        {console.log(profile)}
+        <Img fluid={profile.childImageSharp.fluid} />
       </Box>
     )}
     {!landing || !landing.childMdx || !landing.childMdx.body ? null : (
-      <Box>
+      <Box width={["80%", "50%", "30%"]}>
         <MDXRenderer>{landing.childMdx.body}</MDXRenderer>
       </Box>
     )}
@@ -38,9 +35,12 @@ export default props => (
             body
           }
         }
-        profile: file(relativePath: { eq: "avatar.png" }) {
+        profile: file(
+          sourceInstanceName: { eq: "homepage" }
+          name: { eq: "avatar" }
+        ) {
           childImageSharp {
-            fluid(maxWidth: 500, maxHeight: 500, quality: 90, grayscale: true) {
+            fluid(maxWidth: 500, maxHeight: 500, quality: 90) {
               ...GatsbyImageSharpFluid
             }
           }
