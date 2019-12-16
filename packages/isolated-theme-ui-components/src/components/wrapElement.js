2141557196f98741c25693b7e93e79e2d45d9c3b
Jacob Bolda <me@jacobbolda.com>
2019-12-15 22:01:30 -0600

    remove isolation for the time being

--- packages/isolated-theme-ui-components/src/components/wrapElement.js
+++ packages/isolated-theme-ui-components/src/components/wrapElement.js
@@ -1,13 +1,1 @@
-/** @jsx jsx */
-import { IsolatedThemeContext, jsx } from "../context";
-import theme from "../theme";
-
-export default ({ children }) => (
-  <IsolatedThemeContext.Provider
-    value={{
-      theme
-    }}
-  >
-    {children}
-  </IsolatedThemeContext.Provider>
-);
+export default ({ children }) => children;
