0a26f005f90fc2c7e2637074df30a88cd280c58e
Jacob Bolda <me@jacobbolda.com>
2019-12-15 20:44:01 -0600

    implement interal global

---
+++ packages/gatsby-theme-homepage/gatsby-browser.js
@@ -0,0 +1,1 @@
+export { WrapElement as wrapRootElement } from "@jbolda/isolated-theme-ui-components";
--- packages/isolated-theme-ui-components/src/components/wrapElement.js
+++ packages/isolated-theme-ui-components/src/components/wrapElement.js
@@ -1,15 +1,23 @@
 /** @jsx jsx */
 import { IsolatedThemeContext, jsx } from "../context";
-import { ColorMode } from "theme-ui";
+import { Global } from "@emotion/core";
+import { createColorStyles } from "theme-ui/src/custom-properties";
 import theme from "../theme";
 
-export default ({ children }) => (
+const bodyColor = theme => ({
+  body: createColorStyles(theme)
+});
+
+export default ({ element }) => (
   <IsolatedThemeContext.Provider
     value={{
       theme
     }}
   >
-    <ColorMode />
-    {children}
+    <Global styles={bodyColor} />
+    <div>
+      <span>test text</span>
+    </div>
+    {element}
   </IsolatedThemeContext.Provider>
 );
