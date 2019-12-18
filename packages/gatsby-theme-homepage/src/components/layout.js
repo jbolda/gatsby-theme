261a10da4b0099b28a48b474a3b6e2d3b1b24092
Jacob Bolda <me@jacobbolda.com>
2019-12-18 08:15:55 -0600

    remove 'professional' from 'engagements'

--- packages/gatsby-theme-homepage/src/components/layout.js
+++ packages/gatsby-theme-homepage/src/components/layout.js
@@ -2,13 +2,13 @@ import React from "react";
 import Nav from "@jbolda/gatsby-theme-layout";
 import Landing from "./landing";
 import About from "./about";
-import ProfessionalEngagements from "./professionalEngagements";
+import Engagements from "./engagements";
 
 const Layout = props => (
   <Nav location={props.location}>
     <Landing />
     <About />
-    <ProfessionalEngagements />
+    <Engagements />
     {props.children}
   </Nav>
 );
