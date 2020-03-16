bb1e974effe93a3a16783760fbe5a5d40fc64f2a
Jacob Bolda <me@jacobbolda.com>
2020-03-16 14:55:22 -0500

    update example config with new domain

--- examples/article-social-cards/gatsby-config.js
+++ examples/article-social-cards/gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     siteDescription: `Structural Engineer with a knack for creative solutions using code and ingenuity.`,
     siteAuthor: `Jacob Bolda`,
     siteContact: "https://twitter.com/jacobbolda",
-    siteURL: "https://jbolda-gatsby-theme-article-social-cards.netlify.com/",
+    siteURL: "https://jbolda-gatsby-theme-social.netlify.com/",
     contactLinks: [
       {
         url: "mailto:me@jacobbolda.com",
@@ -110,7 +110,7 @@ module.exports = {
       resolve: `gatsby-theme-recipes`,
       options: {
         siteUrl:
-          "https://jbolda-gatsby-theme-article-social-cards.netlify.com/",
+          "https://jbolda-gatsby-theme-social.netlify.com/",
         sources: ["Airtable"]
       }
     },
