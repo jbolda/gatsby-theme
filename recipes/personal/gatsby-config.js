cefd815120a7734f320164f3e8d84acb96e85975
Jacob Bolda <me@jacobbolda.com>
2020-04-27 11:59:00 -0500

    feat: recipes to get your site spun up _fast_ (#143)
    * upgrade all examples
    
    * add gatsby-cli to root for recipe running
    
    * initial recipe
    
    * add options to config
    
    * backticks cause it to hang
    
    * split up all of the GatsbyPlugin steps
    
    rumor has it that they don't work combined
    
    * it assumes a gatsby-config.js exists, create one manually for now
    
    * GatsbyPlugins can be in the same step
    
    * add Github action to build
    
    using `yes` to pass all the prompts
    
    * move timeout-minutes
    
    * yarn gatsby?
    
    * bump gatsby
    
    * bump gatsby
    
    * run recipe outside of main dir to avoid react errors
    
    * add in recipe templates
    
    * shift cli and try running outside of workspace
    
    * add cconfig that has metadata
    
    * only websites seems to work File
    
    * gatsby script?
    
    * add engagements, update content of existing
    
    * add engagements to the recipe
    
    * fix typo
    
    * add gatsby-plugin-printer as we don't gracefully deal with no social images
    
    yea bugs...
    
    * set overall working dir
    
    * try main gatsby package
    
    * try npx again
    
    * bump gatsby
    
    * ditch dep and try npx in upper folder
    
    * switch test to run in upper folder
    
    * turn off comments for examples for the moment because they are noisy
    
    * add notice about recipes in CI
    
    * yes won't work
    
    * add gitignore
    
    * switch File ref to master
    
    * comma fails yml parse
    
    * try comment instead

---
+++ recipes/personal/gatsby-config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  siteMetadata: {
+    siteTitle: `Jacob Bolda`,
+    siteDescription: `Structural Engineer with a knack for creative solutions using code and ingenuity.`,
+    siteAuthor: `Jacob Bolda`,
+    siteContact: "https://twitter.com/jacobbolda",
+    siteURL: "https://jbolda-gatsby-theme-personal.netlify.com/",
+    contactLinks: [
+      {
+        url: "mailto:me@jacobbolda.com",
+        text: "me@jacobbolda.com",
+        icon: ["far", "envelope"],
+      },
+      {
+        url: "https://twitter.com/jacobbolda",
+        text: "@jacobbolda",
+        icon: ["fab", "twitter"],
+      },
+      {
+        url: "https://linkedin.com/in/bolda",
+        text: "linkedin.com/in/bolda",
+        icon: ["fab", "linkedin"],
+      },
+      {
+        url: "https://github.com/jbolda",
+        text: "github.com/jbolda",
+        icon: ["fab", "github"],
+      },
+      {
+        url: "https://keybase.io/jbolda",
+        text: "keybase.io/jbolda",
+        icon: ["fab", "keybase"],
+      },
+      {
+        url: "https://angel.co/jacobbolda",
+        text: "angel.co/jacobbolda",
+        icon: ["fab", "angellist"],
+      },
+      {
+        url: "http://www.jbolda.com/photo",
+        text: "My Photographs",
+        icon: ["fas", "camera"],
+      },
+    ],
+    navLinks: [
+      { url: "/articles/", text: "Articles" },
+      { url: "/recipes/", text: "Our Recipes" },
+    ],
+  },
+  plugins: [],
+};
