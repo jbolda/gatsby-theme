455dd4e223d836ec728b45abfa83c67c782b4dee
Jacob Bolda <me@jacobbolda.com>
2020-05-04 11:17:55 -0500

    feature: add article reading time (#147)
    * bump packages because broken lockfile
    
    * add timeToRead to articles
    
    * remove gatsby-plugin-printer patch
    
    * resolve puppeteer in root as well
    
    seems resolutions isn't hoisted
    
    * bump gatsby

--- packages/gatsby-theme-articles/src/chrome.js
+++ packages/gatsby-theme-articles/src/chrome.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ArticleLayout from "./articleLayout";
 import FeaturedImage from "./components/featuredImage";
+import ArticleMeta from "./components/articleMeta";
 import ArticleSection from "./components/articleSection";
 
 export default ({ article, location, children }) => (
   <ArticleLayout article={article} location={location}>
     <FeaturedImage featuredImage={article.featuredImage} />
+    <ArticleMeta article={article} />
     <ArticleSection article={article} children={children} />
   </ArticleLayout>
 );
